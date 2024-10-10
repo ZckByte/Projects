96f47678b5b7565b7a64c34e0969942d5c8e5ed4
ZckByte <alanyuzeth68@gmail.com>
2024-10-10 08:22:22 -0500

    Update calcDailyProjectTwo.py

--- Python/DailyProjects/calcDailyProjectTwo.py
+++ Python/DailyProjects/calcDailyProjectTwo.py
@@ -1,21 +1,27 @@
 ope = ["+", "-", "*", "/"]
-print(f"Que operacion deseas realizar {ope}")
-operacion = str(input(""))
-num1 = int(input("Ingrese el primer numero: "))
-num2 = int(input("Ingrese el segundo numero: "))
-
-resultadoS = num1 + num2
-resultadoR = num1 - num2
-resultadoM = num1 * num2
-try:
-    resultadoD = num1 / num2
-except ZeroDivisionError:
-    print("No se puede hacer una división entre cero")
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
 seguir = True
 
+#Bucle de operacion
 while seguir:
+    print(f"Que operacion deseas realizar {ope}")
+    operacion = str(input(""))
+    if operacion in ope:
+        num1 = float(input("Ingrese el primer numero: "))
+        num2 = float(input("Ingrese el segundo numero: "))
+    else:
+        continue
+    resultadoS = num1 + num2
+    resultadoR = num1 - num2
+    resultadoM = num1 * num2
+    while ZeroDivisionError:
+        try:
+            resultadoD = num1 / num2
+            break
+        except ZeroDivisionError:
+            print("No se puede hacer una división entre cero")
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            break
     if operacion == "+":
         print(f"El resultado de {num1} y {num2} es igual a {resultadoS}")
     elif operacion == "-":
@@ -27,7 +33,8 @@ while seguir:
     print("Quieres realizar otra operacion?, S = si, N = no.")
     seguirP = str(input(""))
     seguirP = seguirP.lower()
-    if seguirP != "n":
+    if seguirP != "s":
         seguir=False
     else:
         seguir=True
+print("!Gracias por usar!")
