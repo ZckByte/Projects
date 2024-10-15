6e6b975b8a424e399a38f755207f5a148e10b617
ZckByte <alanyuzeth68@gmail.com>
2024-10-15 08:32:52 -0500

    Create checkNumberDailyProjectFour.py

---
+++ Python/DailyProjects/checkNumberDailyProjectFour.py
@@ -0,0 +1,21 @@
+print("------------------Positive or Negative------------------")
+
+follow=True
+while follow:
+    try:
+        num = float(input("Input the number: "))
+    except:
+        continue
+    if num < 0:
+        print("The number is Negative")
+    elif num > 0:
+        print("The number is Positive")
+    elif num == 0: 
+        print("The number is Zero")
+    follow2 = str(input("wish follow?, S = yes, N = Not: "))
+    follow2 = follow2.lower()
+    if follow2 != "s":
+        print("Thanks for use!")
+        follow=False
+    else:
+        continue
